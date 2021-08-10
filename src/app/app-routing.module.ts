7ccb8e4e795e56f5932da2511d569512255c027c
Hanaa-Kamel <hanaa.kamel.a@gmail.com>
2021-08-10 17:02:02 +0200

    Route Protection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+import { AuthGuard } from './components/auth/auth.guard';
 import { LoginComponent } from './components/auth/login/login.component';
 import { SingupComponent } from './components/auth/singup/singup.component';
 import { TrainingComponent } from './components/training/training.component';
-
 import { WelcomeComponent } from './components/welcome/welcome.component';
 
 const routes: Routes = [
   {path:'', component : WelcomeComponent},
   {path:'singup', component :SingupComponent},
   {path:'login', component: LoginComponent},
-  {path:'trining', component: TrainingComponent}
+  {path:'trining', component: TrainingComponent , canActivate:[AuthGuard]}
 
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
